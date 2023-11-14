4006c04b656b716811d98e460910b07ee8dd4a35
Patrick Fitzgerald <auskast03@gmail.com>
2023-11-14 02:58:55 -0800

    step 2 - input numbers

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 import { Buttons } from "./Buttons";
+import { CalculatorProvider } from "./CalculatorContext";
 import { Container } from "./Container";
 import { Display } from "./Display";
 import { Header } from "./Header";
 
 export const Calculator = () => {
   return (
-    <Container>
-      <Header />
-      <Display />
-      <Buttons />
-    </Container>
+    <CalculatorProvider>
+      <Container>
+        <Header />
+        <Display />
+        <Buttons />
+      </Container>
+    </CalculatorProvider>
   );
 };
--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,24 +1,17 @@
-type Operation = "add" | "sub" | "mult" | "div";
+"use client";
 
-const operations: Record<Operation, string> = {
-  add: "+",
-  sub: "-",
-  mult: "X",
-  div: "/",
-};
+import { useCalculatorContext } from "./CalculatorContext";
 
-export interface DisplayProps {
-  prevValue?: number;
-  operation?: "add" | "sub" | "mult" | "div";
-  input?: number;
-}
+export interface DisplayProps {}
 
-export const Display = ({ prevValue, operation, input = 0 }: DisplayProps) => {
+export const Display = () => {
+  const { input, operator, prevValue } = useCalculatorContext();
+
   return (
     <div className="flex flex-col gap-2 text-right my-4">
       <div className="min-h-[24px]">
         <span className="heading-md">{prevValue}</span>
-        {operation ? operations[operation] : null}
+        {operator}
       </div>
       <div className="heading-lg">{input}</div>
     </div>
